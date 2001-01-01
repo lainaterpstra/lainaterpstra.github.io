02481aac915956d83ba9c2f723ad0f2f0fd21b5e
lainaterpstra <lterpstra@gm.slc.edu>
2000-12-31 16:04:27 -0800

    2017 updates

---
+++ New_Final_Project/2017.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+$('#one').click (function() {
+	$('.big-image').attr('src', '../new_work_oils/2017/Lrg_deHooch_sq.jpg');
+	// $('#OtherCaption').text('Variation from De Hooch A Soldier with Dead Birds. Oil on canvas paper. 2015.')
+})
+
+$('#two').click (function() {
+	$('.big-image').attr('src', '../new_work_oils/2017/Entombment1.jpg');
+	// $('#OtherCaption').text('From Rembrandt Portrait in Meditation. Oil on canvas paper. 16" x 12". 2015.')
+})
+
+$('#three').click (function() {
+	$('.big-image').attr('src', '../new_work_oils/2017/Entombment2.jpg');
+	// $('#OtherCaption').text('From Jacques Louis David The Death Of Socrates. Oil on canvas paper. 2015.')
+})
+
+$('#four').click (function() {
+	$('.big-image').attr('src', '../new_work_oils/2017/Entombment3.jpg');
+	// $('#OtherCaption').text('Untitled (De Hooch Variation) Oil on canvas paper. 23" x 23" 2015.')
+})
+
+
+})
+
+
+
+	
+	
