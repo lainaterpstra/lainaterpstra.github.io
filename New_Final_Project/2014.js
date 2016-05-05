6d429c9719dc9e549c1b0c2d621b635d170c376c
lainaterpstra <lterpstra@gm.slc.edu>
2016-05-04 21:53:49 -0400

    commit

--- New_Final_Project/2014.js
+++ New_Final_Project/2014.js
@@ -3,36 +3,43 @@ $(document).ready(function () {
 
 $('#one').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/Judas_16x20.jpg');
-	
+	$('#OtherCaption').text('16 x 20 Oil on Canvas Paper. 2014')
 })
 
 $('#two').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/sorge_15x19.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 	
 })
 
 $('#three').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/IMG_0321.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('#four').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/Death_of_Socrates_variation_72dpi.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('#five').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/Socrates.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('#six').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/untitled_11.5_11.5.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('#seven').click (function() {
 	$('.big-image').attr('src', '../paintings_cropped/deHooch_cropped.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('#eight').click (function() {
 	$('.big-image').attr('src', '../paintings_cropped/rembrant_square_cropped.jpg');
+	$('#OtherCaption').text('From Pieter De Hooch Woman Drinking With Soldiers Oil on Canvas. 30" x 36"')
 })
 
 $('.fa').click( function () {
--- New_Final_Project/Interiors.js
+++ New_Final_Project/Interiors.js
@@ -9,22 +9,27 @@ $('#one').click (function() {
 
 $('#two').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/Edmund_Tarbell_Commission.jpg');
+	$('#OtherCaption').text('Interpretation of Edmund Tarbells "New England Interior" Oil on Canvas. 40" x 48". 2014.')
 })
 
 $('#three').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/adorationo_Sheep_17x14.jpg');
+	$('#OtherCaption').text('17"x14" Untitled. Oil on Canvas. 2015.')
 })
 
 $('#four').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/Tarbell_study_17x14.jpg');
+	$('#OtherCaption').text('Tarbell Study. Oil On Canvas. 17" x 14". 2014')
 })
 
 $('#five').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/de_hooch_interior.jpg');
+	$('#OtherCaption').text('30" x 24" Oil On Canvas. 2014.')
 })
 
 $('#six').click (function() {
 	$('.big-image').attr('src', '../new_work_oils/room_of_resistance_36x36bw.jpg');
+	$('#OtherCaption').text('"Room of Resistance (Black and White)" Oil on Canvas. 2015.')
 })
 
 
